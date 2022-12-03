0acc50a18a26048cd2ba6c3a18b9e9bf366c0a91
Huri <118631815+NHuriN@users.noreply.github.com>
2022-12-03 13:15:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//alert("Bienvenidos a mi pagina");
+
+//Este es el codigo de una animacion
+let app = document.getElementById('typewriter');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+typewriter
+.pauseFor(2500)
+.typeString('Desarrollo sitios web y escribo articulos sobre programacion')
+.pauseFor(200)
+.deleteChars(10)
+.start();
